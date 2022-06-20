b558c51ec974e1cffeef81055aef20cb75cec988
Alicia Fisher <aliciajadefisher@yahoo.co.uk>
2022-06-20 09:49:51 +0100

    09 - BestAdderEver added

--- C242JavaExercises/src/main/java/Engage/Basics/AllTheMaths.java
+++ C242JavaExercises/src/main/java/Engage/Basics/AllTheMaths.java
@@ -1,4 +1,4 @@
-package Engage;
+package Engage.Basics;
 
 public class AllTheMaths
 {
---
+++ C242JavaExercises/src/main/java/Engage/Variables/BestAdderEver.java
@@ -0,0 +1,19 @@
+package Engage.Variables;
+
+public class BestAdderEver
+{
+    public static void main(String[] args)
+    {
+        // Declare variables
+        int n1 = 1;
+        int n2 = 2;
+        int n3 = 3;
+        int r;
+
+        System.out.println("Numbers: " + n1 + ", " + n2 + ", " + n3);
+
+        // Calculate sum and display
+        r = n1 + n2 + n3;
+        System.out.println("Sum result: " + r);
+    }
+}
